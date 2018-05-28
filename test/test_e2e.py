6b8f8475e204509a087a0513347f4ac29271221d
Ignacio <ignaciolanzi96@gmail.com>
2018-05-27 21:51:01 -0300

    test de integracion y unidad
    test de unidad DELETE y string vacio, test de integracion borrar fila

--- test/test_e2e.py
+++ test/test_e2e.py
@@ -38,6 +38,7 @@ class Ordering(unittest.TestCase):
 
         self.driver = webdriver.Chrome()
 
+"""
     def test_title(self):
         driver = self.driver
         driver.get(self.baseURL)
@@ -45,6 +46,44 @@ class Ordering(unittest.TestCase):
         add_product_button.click()
         modal = driver.find_element_by_id('modal')
         assert modal.is_displayed(), "El modal no esta visible"
+"""
+
+    def test_borrar_fila(self):
+        #Creo los productos
+        prod = Product(id= 1, name= 'Tenedor', price= 50)
+        db.session.add(prod)
+        prod2 = Product(id= 2, name= 'Calabaza', price= 30)
+        db.session.add(prod)
+
+        #Creo una orden
+        order = Order(id= 1)
+        db.session.add(order)
+
+        #Añado los productos a la orden
+        orderProduct = OrderProduct(order_id= 1, product_id= 1, quantity= 1, product= prod)
+        db.session.add(orderProduct)
+        orderProduct = OrderProduct(order_id= 1, product_id= 2, quantity= 1, product= prod2)
+        db.session.add(orderProduct)
+        db.session.commit()
+
+        driver = self.driver
+        driver.get(self.baseURL)
+
+        time.sleep(1)
+
+        delete_product_button = driver.find_element_by_xpath('//*[@id="orders"]/table/tbody/tr/td[6]/button[2]')
+        delete_product_button.click()
+
+        time.sleep(1)
+
+        op = OrderProduct.query.all()
+
+        # Verifica que se haya borrado el producto de la lista de productos
+        self.assertEqual(len(op), 1, "No se borró el producto")
+
+        #Verifica que se haya borrado el producto correcto
+        self.assertNotEqual(op[0].product, prod, "No se borró el producto correcto")
+
 
     def tearDown(self):
         self.driver.get('http://localhost:5000/shutdown')
@@ -56,4 +95,3 @@ class Ordering(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
-
